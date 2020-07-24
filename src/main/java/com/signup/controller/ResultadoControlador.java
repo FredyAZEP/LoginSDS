201ca03f575fdd15c71ce0c18fc0d106b5a8defd
Javier Martinez Becerril <al221810675@gmail.com>
2020-07-24 00:28:32 -0500

    Platilla completa

--- src/main/java/com/signup/controller/ResultadoControlador.java
+++ src/main/java/com/signup/controller/ResultadoControlador.java
@@ -1,20 +1,14 @@
 package com.signup.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
-
 import com.signup.interfaceservice.IresultadoService;
 import com.signup.model.Resultados;
-import com.signup.model.User;
+
 
 @Controller
 @RequestMapping
