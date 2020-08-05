b066958f7abb35f67d66556c89a46ff461cf9c73
Javier Martinez Becerril <al221810675@gmail.com>
2020-08-05 00:23:37 -0500

    Correcion Pro.Noe

--- src/main/java/com/signup/controller/ResultadoControlador.java
+++ src/main/java/com/signup/controller/ResultadoControlador.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import com.signup.interfaceservice.IresultadoService;
 import com.signup.model.Resultados;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 @Controller
@@ -16,18 +18,24 @@ public class ResultadoControlador {
 	
 	@Autowired
 	private IresultadoService service;
+	 private static Logger log = LoggerFactory.getLogger(ResultadoControlador.class);
 	
-	
-	
-		
-		
-	
-	 
 	 @RequestMapping(value= {"/home/74Frac1"}, method=RequestMethod.GET)
 	 public String listar3(Model model) {
+		 String auxiliar = "";
 	  List<Resultados>resultado=service.listar3();
-		 model.addAttribute("resultados" , resultado);
-	  return "/home/74Frac1";
+	  int num_registros = resultado.size();
+	  log.info(auxiliar);
+	  if(resultado.isEmpty()) {
+		  model.addAttribute("resultados" , resultado);
+			 auxiliar =  "/home/74Frac1";
+			
+		}else {
+			 auxiliar = "error";
+			
+		}
+		return auxiliar;
+		
 	 }
 
 }
